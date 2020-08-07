07eb684478c0ca6fb9f51041d96d5f756063dcb4
WAQAS <waqas.attary@gmail.com>
2020-08-07 22:43:58 +0500

    Receal on scroll is completed

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,13 @@
-import '../styles/style.css';
-import RevealOnScroll from './modules/RevealOnScroll';
-import MobileMenu from './modules/MobileMenu';
+import '../styles/style.css'
+import RevealOnScroll from './modules/RevealOnScroll'
+import MobileMenu from './modules/MobileMenu'
 
 
-let mobileMenu = new MobileMenu();
-let revealOnScroll = new RevealOnScroll();
+let mobileMenu = new MobileMenu()
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 if (module.hot) {
-    module.hot.accept();
+    module.hot.accept()
 }
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,22 +1,60 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = document.querySelectorAll(".feature-item");
-        this.hideInitially();
-        this.events();
+
+    constructor(els, tresholdPercent) {      //Elements params
+        this.tresholdPercent = tresholdPercent
+        this.itemsToReveal = els
+        this.browserHeight = window.innerHeight;
+        this.hideInitially()
+
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+
+        this.events()
     }
 
     events() {
-        window.addEventListener("scroll", () => {
-            this.itemsToReveal.forEach(e => {
+        window.addEventListener("scroll", this.scrollThrottle)
+
+        window.addEventListener("resize", debounce(() => {
+            console.log("resize")
+            this.browserHeight = window.innerHeight
+        }, 333))
+    }
+
+    calcCaller() {
+        console.log('Scrolling')
+        this.itemsToReveal.forEach( el => {
+            if(!el.isRevealed) {
+                this.calculateIfScrollTo(el);
+            }
+        })
+    }
+
+    calculateIfScrollTo(el) {
+        if(window.scrollY + this.browserHeight> el.offsetTop) {
+            console.log('Calculate')
+            let scrollPersent = (el.getBoundingClientRect().y / this.browserHeight)  * 100;
+            if(scrollPersent < this.tresholdPercent) {
+                el.classList.add('reveal-item--is-visible')
+                el.isRevealed = true
+                if(el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle)
+                }
                 
-            })
-        });
+            }
+        }
     }
 
     hideInitially() {
-        this.itemsToReveal.forEach( e => e.classList.add("reveal-item") );
+        this.itemsToReveal.forEach ( el => {
+            el.classList.add("reveal-item") 
+            el.isRevealed = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length -1].isLastItem = true
     }
 }
 
 
-export default RevealOnScroll;
+export default RevealOnScroll
